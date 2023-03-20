af26ac92501d7d2d18b59e34d6c0f715963e0db1
Denita Jefferson <83525597+Denita06@users.noreply.github.com>
2023-03-20 16:20:59 -0400

    Create Hangman.java

---
+++ src/main/java/Hangman.java
@@ -0,0 +1,46 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+import java.util
+import java util.Random
+import java.util.Dictionary
+	
+	
+public class Hangman{
+  private int numberOfGamesWon;
+  private int numberOfGamesPlayed;
+  private int totalPoints 
+  private String userName
+
+
+  public Hangman(){
+  	return nul;
+  }
+
+  public int getNumberOfGamesWon(){
+  	return null;
+	}
+
+  public int setNumberOfGamesWon(){
+	return null:
+	}
+	
+  public void setTheme(){
+	return null:
+	}
+	
+  private void letterClicked(Letter actor){
+	return null:
+	}
+	
+  public void act(){
+	return null:
+	}
+
+  public letter(){
+	return null:
+	} 
+
+  public void pick(){
+	return null:
+	}
+}
