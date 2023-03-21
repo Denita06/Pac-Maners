d5ba65eec8c02e48125dc5fea191db08bfb90a2e
denitajefferson <denitajefferson04@students.desu.edu>
2023-03-20 21:29:44 -0400

    created userProfile

--- userProfile.java
+++ userProfile.java
@@ -8,14 +8,63 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class userProfile extends World
 {
-
+    private int totalPoints;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
     /**
      * Constructor for objects of class userProfile.
      * 
      */
-    public userProfile()
+    public userProfile(String first, String last, String user, String pass)
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(600, 400, 1);
+        
+        this.firstName = first;
+        this.lastName = last;
+        this.username = user;
+        this.password = pass;
+    }
+   
+    //set first name
+    public void setFirstName(String firstName){
+        this.firstName = firstName;
+    }
+    
+    //get first name
+    public String getFirstName(){
+        return firstName;
+    }
+    
+    //set last name
+    public void setLastName(String LastName){
+        this.firstName = LastName;
+    }
+    
+    //get last name
+    public String getLastName(){
+        return lastName;
+    }
+    
+    //set user name
+    public void setUserName(String UserName){
+        this.firstName = UserName;
+    }
+    
+    //get user name
+    public String getUserName(){
+        return username;
+    }
+    
+    //set password
+    public void setPassword(String password){
+        this.password = password;
+    }
+    
+    //get password
+    public String getPassword(){
+        return password;
     }
 }
