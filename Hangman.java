098a16354abb0586d8cb42a7ccfffb2885d4c781
Denita Jefferson <83525597+Denita06@users.noreply.github.com>
2023-03-20 20:23:44 -0400

    Add files via upload

---
+++ Hangman.java
@@ -0,0 +1,48 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Hangman here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+
+import java.util.*;
+
+public class Hangman extends World
+{
+    private int numberOfGanmesWon;
+    private int numberOfGamesPlayed;
+    private int totalPoints;
+    private String userName;
+    
+    /**
+     * Constructor for objects of class Hangman.
+     * 
+     */
+    public Hangman()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+    
+    public int getNumberOfGamesWon(){
+        return 0;
+    }
+
+    public int SetNumberOfGamesWon(){
+        return 0;
+    }
+    
+    public void letterClicked(){
+    }
+    
+    public void act(){
+    }
+    
+    public void pick(){
+    }
+    
+    public void letter(){
+    }
+}
---
+++ theme.java
@@ -0,0 +1,37 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class theme here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class theme extends World
+{
+
+    /**
+     * Constructor for objects of class theme.
+     * 
+     */
+    public theme()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+    
+    public void backgroundColor(){
+
+    }
+    
+    public void font(){
+        
+    }
+    
+    public static void playMusic(){
+        
+    }
+    
+    public static void pauseMusic(){
+        
+    }
+}
